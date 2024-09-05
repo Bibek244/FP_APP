465530b12db9631725accb6de17dd6eed21466a0
bibek244 <bibekgosai555@gmail.com>
2024-09-05 11:03:44 +0000

    feat(Goods):  update-goods-migration logic in service class

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,10 +2,12 @@
 
 module Types
   class MutationType < Types::BaseObject
-
+    # product or goods mutations
     field :create_goods, mutation: Mutations::Goods::CreateGoods
+    field :update_goods, mutation: Mutations::Goods::UpdateGoods
+
+    # user registrations and login mutations
     field :register_user, mutation: Mutations::Users::RegisterUser
     field :login_user, mutation: Mutations::Users::LoginUser
-
   end
 end
