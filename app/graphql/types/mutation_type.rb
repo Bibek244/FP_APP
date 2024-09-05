916c44e8c02aa1d67d0b850d03704051ccf49a11
tsuxan <manandhartsuxan@gmail.com>
2024-09-05 04:43:20 +0000

    Revert "Merge pull request #6 from Bibek244/feature/user-registration-login"
    This reverts commit a0ffd2e28407c2164436e92ce00245b999f6b96a, reversing
    changes made to e1e3564d6924264ad2ca78b9239b2b59f9b706a7.

--- app/graphql/types/mutation_type.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  class MutationType < Types::BaseObject
-    field :register_user, mutation: Mutations::Users::RegisterUser
-    field :login_user, mutation: Mutations::Users::LoginUser
-  end
-end
