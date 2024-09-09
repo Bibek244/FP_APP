64361ebd0f8a033eb189cf0e4ac33bf29b43c3a0
tsuxan <manandhartsuxan@gmail.com>
2024-09-09 04:20:07 +0000

    Feat: Querry added for specific Driver

---
+++ app/graphql/resolvers/driver/specific_driver.rb
@@ -0,0 +1,13 @@
+class ::Resolvers::Driver::SpecificDriver < Resolvers::BaseResolver
+  argument :driver_id, ID, required: true
+
+  type Types::DriverType, null: true
+
+  def resolve(driver_id:)
+    driver = ::Driver.find_by(id: driver_id)
+    if driver.nil?
+      raise GraphQL::ExecutionError, "Driver with ID #{driver_id} not found"
+    end
+    driver
+  end
+end
