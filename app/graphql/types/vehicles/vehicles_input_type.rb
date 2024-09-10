9b0b71b8c8192e5ed694bc6a7c919538f752be57
bibek244 <bibekgosai555@gmail.com>
2024-09-10 04:52:27 +0000

    feat(vehicle): create graphql objects for vehicle

---
+++ app/graphql/types/vehicles/vehicles_input_type.rb
@@ -0,0 +1,12 @@
+module Types
+  module Vehicles
+    class VehiclesInputType < Types::BaseInputObject
+      argument :license_plate, String, required: true
+      argument :brand, String, required: true
+      argument :vehicle_type, String, required: false
+      argument :status, StatusType, required: true
+      argument :capacity, Integer, required: true
+      argument :group_id, ID, required: true
+    end
+  end
+end
---
+++ app/graphql/types/vehicles/vehicles_type.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Types
+  module Vehicles
+    class VehiclesType < Types::BaseObject
+      field :id, ID, null: false
+      field :license_plate, String, null: true
+      field :brand, String, null: true
+      field :vehicle_type, String, null: true
+      field :status, ::Types::Vehicles::StatusType, null: false
+      field :capacity, Integer, null: true
+      field :group_id, ID, null: false
+    end
+  end
+end
