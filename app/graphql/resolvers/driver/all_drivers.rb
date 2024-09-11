e1c29298fb87c7b28c63fef372da8a1d658eddfb
tsuxan <manandhartsuxan@gmail.com>
2024-09-11 07:37:17 +0000

    Chore: resolver for alldrivers corrected

--- app/graphql/resolvers/driver/all_drivers.rb
+++ app/graphql/resolvers/driver/all_drivers.rb
@@ -1,10 +1,7 @@
 class ::Resolvers::Driver::AllDrivers < Resolvers::BaseResolver
-  type Types::DriverType, null: false
+  type [ Types::DriverType ], null: false
 
   def resolve
-    drivers = Driver.all
-    debugger
-    Rails.logger.debug("Drivers: #{drivers.inspect}")
-    drivers
+    Driver.all
   end
 end
