e900a7d03c7c7aaff1d246acd11447325932a6d8
tsuxan <manandhartsuxan@gmail.com>
2024-09-11 10:44:28 +0000

    Chore:Validation for Customer added

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,7 @@ class Customer < ApplicationRecord
   belongs_to :group
   validates :email, presence: true, uniqueness: { scope: :group_id }
   validates :phone, presence: true, uniqueness: { scope: :group_id }
+  validates :name, presence: true
+  validates :address, presence: true
   has_many :customer_branches
 end
