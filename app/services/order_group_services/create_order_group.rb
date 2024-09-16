7b3542f06561c6e843963bd6db7d93ccf3917c93
bibek244 <bibekgosai555@gmail.com>
2024-09-16 10:54:05 +0000

    feat(feature/recurring_order): add logic in create_order_service_class to support recurring order

--- app/services/order_group_services/create_order_group.rb
+++ app/services/order_group_services/create_order_group.rb
@@ -26,6 +26,7 @@
       private
 
       def call
+        binding.irb
         ActiveRecord::Base.transaction do
           customer_branch = CustomerBranch.find_by(id: @create_order[:customer_branch_id])
           unless customer_branch
@@ -43,7 +44,11 @@
             group_id: @create_order[:group_id],
             planned_at: @create_order[:planned_at],
             customer_id: customer,
-            customer_branch_id: customer_branch.id
+            customer_branch_id: customer_branch.id,
+            recurring: @create_order[:recurring],
+            recurrence_frequency: @create_order[:recurrence_frequency],
+            next_due_date: @create_order[:next_due_date],
+            recurrence_end_date: @create_order[:recurrence_end_date]
           )
           @create_order[:lined_items_attributes].each do |item_attributes|
             line_item = @order_group.line_items.find_by(id: item_attributes[:id])
