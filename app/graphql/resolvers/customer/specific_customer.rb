44f6b5982c095756900ce6d4fa34d06923102e84
tsuxan <manandhartsuxan@gmail.com>
2024-09-19 09:05:50 +0000

    Revised the resolver for  customer/customer_branch/driver/goods and vehicles using ActsAstenant

--- app/graphql/resolvers/customer/specific_customer.rb
+++ app/graphql/resolvers/customer/specific_customer.rb
@@ -5,7 +5,8 @@ class ::Resolvers::Customer::SpecificCustomer < Resolvers::BaseResolver
 
   def resolve(customer_id:)
     authorize
-
+    current_user = context[:current_user]
+    ActsAsTenant.current_tenant = current_user.group
     ::Customer.find_by(id: customer_id)
   end
 end
--- app/graphql/resolvers/driver/all_drivers.rb
+++ app/graphql/resolvers/driver/all_drivers.rb
@@ -1,10 +1,11 @@
 class ::Resolvers::Driver::AllDrivers < Resolvers::BaseResolver
   type [ Types::Driver::DriverType ], null: false
 
-  argument :group_id, ID, required: true
-  def resolve(group_id:)
+  # argument :group_id, ID, required: true
+  def resolve
     authorize
-
-    Driver.where(group_id: group_id)
+    current_user = context[:current_user]
+    ActsAsTenant.current_tenant = current_user.group
+    Driver.all.order(created_at: :desc)
   end
 end
