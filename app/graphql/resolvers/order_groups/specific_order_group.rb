1eff95007e211c510e99b606218983a60281a71b
bibek244 <bibekgosai555@gmail.com>
2024-09-22 18:37:11 +0000

    feat(feature/recurring): Add ActsAsTenant to  resolvers for tenant scoping

--- app/graphql/resolvers/order_groups/specific_order_group.rb
+++ app/graphql/resolvers/order_groups/specific_order_group.rb
@@ -4,6 +4,10 @@ class Resolvers::OrderGroups::SpecificOrderGroup < Resolvers::BaseResolver
   argument :id, ID, required: true
 
   def resolve(id:)
+    authorize
+    current_user = context[:current_user]
+    ActsAsTenant.current_tenant = current_user.group
+
     order_group = OrderGroup.find_by(id: id)
     if order_group.present?
       { order: order_group, line_items: order_group.line_items, customer: order_group.customer, customer_branch: order_group.customer_branch, message: "Successfully fetched the group_group.", errors: [] }
