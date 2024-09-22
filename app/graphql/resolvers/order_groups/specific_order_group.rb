1ec10b7a77eaa63de22306a713c757489716b30b
bibek244 <bibekgosai555@gmail.com>
2024-09-22 18:52:12 +0000

    fix(feature/delivery_order): fixed resolver for delivery_order and added acts as tenant

--- app/graphql/resolvers/order_groups/specific_order_group.rb
+++ app/graphql/resolvers/order_groups/specific_order_group.rb
@@ -12,7 +12,7 @@ class Resolvers::OrderGroups::SpecificOrderGroup < Resolvers::BaseResolver
     if order_group.present?
       { order: order_group, line_items: order_group.line_items, customer: order_group.customer, customer_branch: order_group.customer_branch, message: "Successfully fetched the group_group.", errors: [] }
     else
-      { order: order_group, message: nil, errors: [ "error: failed to fetch group" ] }
+      { order: nil, message: nil, errors: [ "error: failed to fetch group" ] }
     end
   end
 end
