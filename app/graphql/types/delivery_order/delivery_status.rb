d79cd0e427cb7c884789d11bde8e183555da95e6
bibek244 <bibekgosai555@gmail.com>
2024-09-22 18:24:09 +0000

    refactor(feature/recurring): refactor DeliveryOrder status field and driver status filed to use enum instead of string

--- app/graphql/types/delivery_order/delivery_status.rb
+++ app/graphql/types/delivery_order/delivery_status.rb
@@ -3,8 +3,8 @@ module Types
     class DeliveryStatus < Types::BaseEnum
       value "PENDING", "The order is pending and soon will be dispatched", value: "pending"
       value "ON_THE_WAY", "The order is dispatched and is on the way", value: "on_the_way"
-      value "DELIVERED", "The order is successfully deliverd.", value: "deliverd"
-      value "CANCELLED", " The order is cancelled.", value: "cancelled"
+      value "DELIVERED", "The order is successfully delivered.", value: "delivered"
+      value "CANCELLED", "The order is cancelled.", value: "cancelled"
     end
   end
 end
