2c4c856de53e84b2b841978666ccc9315f4418af
bibek244 <bibekgosai555@gmail.com>
2024-09-23 09:54:03 +0000

    fix(feature/category): fixed all_category

--- app/graphql/types/category/category_result_type.rb
+++ app/graphql/types/category/category_result_type.rb
@@ -1,7 +1,7 @@
 module Types
   module Category
     class CategoryResultType < Types::BaseObject
-      field :category, Types::Category::CategoryType, null: true
+      field :category, [ Types::Category::CategoryType ], null: true
       field :errors, [ String ], null: true
       field :message, String, null: true
     end
