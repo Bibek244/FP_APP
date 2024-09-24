b0f85cf7b003f8fd91469f98c87057132a324476
bibek244 <bibekgosai555@gmail.com>
2024-09-24 10:59:26 +0000

    fix(feature/order_group): move line_items from order group to deliver_order

--- app/sidekiq/recurring_order_job.rb
+++ app/sidekiq/recurring_order_job.rb
@@ -31,23 +31,14 @@ class RecurringOrderJob
         group_id: parent_order_group.group_id,
         customer_id: parent_order_group.customer_id,
         customer_branch_id: parent_order_group.customer_branch_id,
-        recurring: parent_order_group.recurring,
-        recurrence_frequency: parent_order_group.recurrence_frequency,
-        next_due_date: next_due_date,
-        recurrence_end_date: parent_order_group.recurrence_end_date
+        recurring: false
       )
-
-      parent_order_group.line_items.each do |parent_line_item|
-        child_order_group.line_items.create!(
-          parent_line_item.attributes.except("id", "created_at", "updated_at", "order_group_id")
-        )
-      end
 
       child_order_group
     end
 
   def create_delivery_order(child_order_group, next_due_date, delivery_order)
-    DeliveryOrder.create!(
+    new_delivery_order = DeliveryOrder.create!(
       order_group_id: child_order_group.id,
       vehicle_id: delivery_order[:vehicle_id],
       group_id: delivery_order[:group_id],
@@ -57,6 +48,12 @@ class RecurringOrderJob
       customer_branch_id: delivery_order[:customer_branch_id],
       delivery_date: next_due_date
     )
+
+    delivery_order.line_items.each do |parent_line_item|
+    new_delivery_order.line_items.create!(
+      parent_line_item.attributes.except("id", "create_at", "updated_at", "order_group_id")
+    )
+    end
   end
 
   def calculate_next_due_date(order_group)
