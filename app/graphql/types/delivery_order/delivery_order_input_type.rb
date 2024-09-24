178446bbc09673bd5ebd52869edc948c47c0fd7b
bibek244 <bibekgosai555@gmail.com>
2024-09-24 10:54:58 +0000

    fix(feature/order_group): move line_items from order group to deliver_order

--- app/graphql/types/delivery_order/delivery_order_input_type.rb
+++ app/graphql/types/delivery_order/delivery_order_input_type.rb
@@ -5,11 +5,11 @@ module Types
       argument :customer_branch_id, ID, required: false
       argument :driver_id, ID, required: true
       argument :vehicle_id, ID, required: true
-      # argument :order_group_id, ID, required: false
-      argument :lined_items_attributes, [ Types::OrderGroups::LineItemInputType ], required: true
+      argument :order_group_id, ID, required: false
+      argument :lined_items_attributes, [ Types::LineItem::LineItemInputType ], required: true
       argument :status, Types::DeliveryOrder::DeliveryStatus, required: true
-      argument :dispatched_date, GraphQL::Types::ISO8601Date, required: false
-      argument :delivery_date, GraphQL::Types::ISO8601Date, required: false
+      argument :dispatched_date, GraphQL::Types::ISO8601Date
+      argument :delivery_date, GraphQL::Types::ISO8601Date
     end
   end
 end
