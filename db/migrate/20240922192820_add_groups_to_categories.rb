674a182dc05af9ccc86b4a34e9b42e5b989b5e3d
bibek244 <bibekgosai555@gmail.com>
2024-09-25 13:12:00 +0000

    feat(feature/category): add groups to category

--- db/migrate/20240922192820_add_groups_to_categories.rb
+++ db/migrate/20240922192820_add_groups_to_categories.rb
@@ -1,4 +1,4 @@
-class AddGropsToCategories < ActiveRecord::Migration[7.2]
+class AddGroupsToCategories < ActiveRecord::Migration[7.2]
   def change
     add_reference :categories, :group, null: false, foreign_key: true
   end
