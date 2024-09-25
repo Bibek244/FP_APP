afe61802933e12c74c7d3912b79b9ee2a13d5b9e
bibek244 <bibekgosai555@gmail.com>
2024-09-25 13:27:24 +0000

    feat(fix/validation): add validation and set dependent destroy for associations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,13 @@
 class Customer < ApplicationRecord
   acts_as_tenant(:group)
 
-  # belongs_to :group
+  has_many :customer_branches, dependent: :destroy
+  has_many :order_groups, dependent: :destroy
+  has_many :delivery_order, dependent: :destroy
+
   validates :email, presence: true, uniqueness: { scope: :group_id }
-  validates :phone, presence: true, uniqueness: { scope: :group_id }
-  validates :name, presence: true
+  validates :phone, presence: true, uniqueness: { scope: :group_id }, length: { is: 10 }
+  validates :email, format: { with: Devise.email_regexp }
+  validates :name, presence: true, length: { minimum: 3 }
   validates :address, presence: true
-  has_many :customer_branches
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,6 @@
 class LineItem < ApplicationRecord
   belongs_to :goods
   belongs_to :delivery_order
+
   enum unit: { liters: 0, gallons: 1 }
 end
