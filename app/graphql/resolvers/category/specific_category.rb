7d861b99467a562b1cb4f72f7d9188aebab7308e
bibek244 <bibekgosai555@gmail.com>
2024-09-25 10:33:18 +0000

    fix(feature/category): fix typo

--- app/graphql/resolvers/category/specific_category.rb
+++ app/graphql/resolvers/category/specific_category.rb
@@ -1,17 +1,18 @@
-class ::Resolvers::Category::SpecificCategory < Resolvers::BaseResolver
+class Resolvers::Category::SpecificCategory < Resolvers::BaseResolver
   type Types::Category::CategoryResultType, null: true
 
   argument :id, ID, required: true
 
   def resolve(id:)
+    binding.irb
     authorize
     current_user = context[:current_user]
     ActsAsTenant.current_tenant = current_user.group
-    category = Category.find_by(id: id)
+    categories = Category.find_by(id: id)
     if categories.nil?
       { category: nil, message: "failed to fetch category", errors: [ "Category with ID #{id} not found" ] }
     else
-      { category: category, message: "succesfully fetched category", errors: [] }
+      { category: [ categories ], message: "succesfully fetched category", errors: [] }
     end
   end
 end
