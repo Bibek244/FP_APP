6b77c840549a1e289791fa2ff8c110035f34ab3d
bibek244 <bibekgosai555@gmail.com>
2024-09-25 13:22:23 +0000

    feat(feature/csv): csv exporter

---
+++ app/graphql/resolvers/csv/csv_export_resolver.rb
@@ -0,0 +1,20 @@
+class Resolvers::Csv::CsvExportResolver < Resolvers::BaseResolver
+  type String, null: false
+
+  argument :id, ID, required: true
+
+  def resolve(id:)
+    customer = Customer.find_by(id: id)
+    unless customer
+      raise GraphQL::ExecutionError, "Customer not found."
+    end
+
+    file_name = "export_#{Time.now.to_i}.csv"
+    csv_data = ::CsvServices::CsvGeneratorServices.new(customer.id).generate_csv
+    file_path = Rails.root.join("public", "csv_exports", file_name)
+    FileUtils.mkdir_p(Rails.root.join("public", "csv_exports"))
+    File.write(file_path, csv_data)
+
+    "/public/csv_exports/#{file_name}"
+  end
+end
