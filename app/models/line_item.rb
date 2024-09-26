04e4359183eb7eba008768a0c2c0bf31bc4742e7
bibek244 <bibekgosai555@gmail.com>
2024-09-26 04:12:01 +0000

    fix(feature/line_item): add validation for qunatity

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,7 @@ class LineItem < ApplicationRecord
   belongs_to :delivery_order
 
   enum unit: { liters: 0, gallons: 1 }
+
+  validates :quantity, numericality: { greater_than: 0 }, presence: true
+  validates :unit, :goods_id, presence: true
 end
