88695f1c83fdf0fd574c3a762377209e30a8076d
bibek244 <bibekgosai555@gmail.com>
2024-09-29 12:16:37 +0000

    refactor(feature/vehicle): change status enum from integer to string

--- app/models/concerns/status.rb
+++ app/models/concerns/status.rb
@@ -1,7 +1,7 @@
 module Status
   extend ActiveSupport::Concern
   included do
-    enum status: { available: 0, in_use: 1, maintenance: 2 }
+    enum status: { available: "AVILABLE", in_use: "IN_USE", maintenance: "MAINTENANCE" }
 
 
     validates :status, presence: true
