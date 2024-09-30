321565e3a6b57d2a5a8d2b1ce4c6322514a8d69e
bibek244 <bibekgosai555@gmail.com>
2024-09-30 05:47:24 +0000

    feat(feature/driver): Add acts_as_paranoid for soft delete functionality in driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,8 +1,10 @@
 class Driver < ApplicationRecord
   # belongs_to :group
   acts_as_tenant(:group)
+  acts_as_paranoid
+  has_many :delivery_orders
 
-  has_many :delivery_orders, dependent: :destroy
+  before_destroy :nullify_delivery_orders_driver_id
 
   enum status: { AVAILABLE: "AVAILABLE", UNAVAILABLE: "UNAVAILABLE", DEPLOYED: "DEPLOYED" }
 
@@ -10,4 +12,14 @@ class Driver < ApplicationRecord
   validates :phone_no, presence: true, numericality: true, length: { is: 10 }, uniqueness: { scope: :group_id }
   validates :name, presence: true, length: { minimum: 3 }
   validates :address, presence: true, length: { minimum: 5 }
+
+
+  private
+
+  def nullify_delivery_orders_driver_id
+    delivery_orders.where(status: "pending").update_all(driver_id: nil)
+    update(status: "UNAVAILABLE") if status != "UNAVAILABLE"
+  end
+
+  scope :active, -> { where(deleted_at: nil) }
 end
