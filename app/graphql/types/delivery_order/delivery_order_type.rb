1daaa3ec67456e1d65b2de1313941e69f852e7cc
bibek244 <bibekgosai555@gmail.com>
2024-10-01 10:25:32 +0000

    feat(feature/vehicle): add active and all with deleted resolver for vehicle

--- app/graphql/types/delivery_order/delivery_order_type.rb
+++ app/graphql/types/delivery_order/delivery_order_type.rb
@@ -8,7 +8,7 @@ module Types
       field :customer_id, Integer, null: false
       field :customer_branch_id, Integer, null: false
       field :driver, Types::Driver::DriverType, null: true
-      field :vehicle, Types::Vehicles::VehiclesType, null: false
+      field :vehicle, Types::Vehicles::VehiclesType, null: true
       field :order_group_id, Integer, null: false
       field :line_items, [ Types::LineItem::LineItemType ], null: true
       field :status, Types::DeliveryOrder::DeliveryStatus, null: false
