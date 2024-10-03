4d8eecd0d82474392709a5aeb4a9cc773ae1e906
tsuxan <manandhartsuxan@gmail.com>
2024-10-03 11:45:28 +0000

    relocate: factory bot support file relocated

--- spec/support/membership.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :membership do
-    association :group
-    association :user
-  end
-end
--- spec/support/user.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    email { "abc@mail.com" }
-    password { "test@password" }
-    password_confirmation { "test@password" }
-  end
-end
